a1b76c94e7e5164314325283173c811bd0ce7532
devSecD <dev.sec.d@gmail.com>
2024-04-10 17:55:45 -0600

    Add the CSS that was missing from the form and modify the migration of the projects because it had the code that Laravel returns by default

--- database/migrations/2024_04_10_230233_create_projects_table.php
+++ database/migrations/2024_04_10_230233_create_projects_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            // $table->foreignIdFor(User::class);
+            $table->string('name', 100);
+            $table->text('description');
+            $table->timestamps(); // con este creamos las columnas "created_at" y "updated_at"
         });
     }
 
